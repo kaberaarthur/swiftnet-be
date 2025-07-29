d893bcad01116cd6af59e4711b2e302e448783e7
kaberaarthur <arthurkabera@gmail.com>
2025-07-29 11:33:43 +0300

    Necessary changes

--- dbPromise.js
+++ dbPromise.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 const os = require('os');
+require('dotenv').config();
 
 // Get the server's IP address
 const serverIP = os.networkInterfaces()['eth0']?.find(interface => interface.family === 'IPv4')?.address || 'localhost';
@@ -10,9 +11,9 @@ const host = (serverIP === '139.59.60.20') ? 'localhost' : '139.59.60.20';
 // MySQL connection setup
 const pool = mysql.createPool({
     host: host,  // Dynamically set the host based on the server IP
-    user: 'swiftnet',
-    password: 'nOIqSz3aGgYM9z7J',
-    database: 'swiftnet',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     connectTimeout: 10000,
